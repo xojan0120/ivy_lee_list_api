0cbdddfa4a0cb790738ad078f5d0f6ca0deecc2b
xojan0120 <xojan0120@gmail.com>
2019-03-15 09:34:27 +0900

    [Update] リファクタリング中

--- app/controllers/api/v1/archived_items_controller.rb
+++ app/controllers/api/v1/archived_items_controller.rb
@@ -3,23 +3,23 @@ module Api
     class ArchivedItemsController < ApplicationController
       def index
         items = ArchivedItem.select(:id, :title).order(order_number: :asc)
-        render json: { status: 'SUCCESS', message: 'loaded archived items', data: items }
+        render json: { status: 200, message: 'loaded archived items', data: items }
       end
 
       def restore
         deleted_item = ArchivedItem.delete_item(params['item_id'])
         added_item = Item.add_item(deleted_item.title)
-        render json: { status: 'SUCCESS', message: 'restored item', data: added_item }
+        render json: { status: 200, message: 'restored item', data: added_item }
       end
 
       def destroy
         ArchivedItem.delete_item(params['item_id'])
-        render json: { status: 'SUCCESS', message: 'deleted item', data: {} }
+        render json: { status: 200, message: 'deleted item', data: {} }
       end
 
       def destroy_all
         ArchivedItem.delete_all
-        render json: { status: 'SUCCESS', message: 'deleted all archived item', data: {} }
+        render json: { status: 200, message: 'deleted all archived item', data: {} }
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  # rescue_fromは後に書いたもののほうが優先される
+  # railsガイドではrescue_fromにExceptionを指定するのは非推奨。
+  # https://railsguides.jp/action_controller_overview.html#rescue-from
+
+  #rescue_from Exception,                      with: :render_500
+  rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+
+  def render_404(e)
+    render_response(e, 404, 'Not found error')
+  end
+
+  private
+
+    def render_response(e, status, message)
+      logger.error "render_#{status}: #{message}"
+      logger.error e.backtrace.join("\n")
+      render status: status, json: { status: status, message: message }
+    end
+
 end
